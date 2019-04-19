5b2367cb643f558d178da51f37f90c6b7026295f
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-04-19 21:35:33 +0200

    a lot

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package matrixbot
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/matrix-org/gomatrix"
@@ -15,6 +16,7 @@ type MatrixBot struct {
 	matrixPass string
 	matrixUser string
 	Handlers   []CommandHandler
+	Name       string
 }
 
 //CommandHandler struct to hold a pattern/command asocciated with the
@@ -28,6 +30,9 @@ type CommandHandler struct {
 
 	//The function to handle this command
 	Handler func(message, room, sender string)
+
+	//Help to be displayed for this command
+	Help string
 }
 
 func (gb *MatrixBot) getSenderPower(sender string) int {
@@ -39,31 +44,32 @@ func (gb *MatrixBot) getSenderPower(sender string) int {
 }
 
 //RegisterCommand allows to register a command to a handling function
-func (gb *MatrixBot) RegisterCommand(pattern string, minpower int, handler func(message string, room string, sender string)) {
+func (bot *MatrixBot) RegisterCommand(pattern string, minpower int, help string, handler func(message string, room string, sender string)) {
 	mbch := CommandHandler{
-		Pattern:  pattern,
+		Pattern:  bot.Name + " " + pattern,
 		MinPower: minpower,
 		Handler:  handler,
+		Help:     help,
 	}
-	fmt.Println("Registered command: " + pattern)
-	gb.Handlers = append(gb.Handlers, mbch)
+	fmt.Println("Registered command: " + mbch.Pattern)
+	bot.Handlers = append(bot.Handlers, mbch)
 }
 
-func (gb *MatrixBot) handleCommands(message, room, sender string) {
+func (bot *MatrixBot) handleCommands(message, room, sender string) {
 
 	//Don't do anything if the sender is the bot itself
 	//TODO edge-case: bot has the same name as a user but on a different server
-	if strings.Contains(sender, gb.matrixUser) {
+	if strings.Contains(sender, bot.matrixUser) {
 		return
 	}
 
-	for _, v := range gb.Handlers {
+	for _, v := range bot.Handlers {
 		r, _ := regexp.Compile(v.Pattern)
 		if r.MatchString(message) {
-			if v.MinPower <= gb.getSenderPower(sender) {
+			if v.MinPower <= bot.getSenderPower(sender) {
 				v.Handler(message, room, sender)
 			} else {
-				gb.SendToRoom(room, "You have not enough power to execute this command ("+v.Pattern+"). Your power: "+string(gb.getSenderPower(sender))+", requeired: "+string(v.MinPower))
+				bot.SendToRoom(room, "You have not enough power to execute this command ("+v.Pattern+"). Your power: "+string(bot.getSenderPower(sender))+", requeired: "+string(v.MinPower))
 			}
 		}
 	}
@@ -92,7 +98,7 @@ func (bot *MatrixBot) Sync() {
 }
 
 //NewMatrixBot creates a new bot form user credentials
-func NewMatrixBot(user, pass string) (*MatrixBot, error) {
+func NewMatrixBot(user, pass string, name string) (*MatrixBot, error) {
 
 	fmt.Println("Logging in")
 
@@ -114,6 +120,7 @@ func NewMatrixBot(user, pass string) (*MatrixBot, error) {
 		matrixPass: pass,
 		matrixUser: user,
 		Client:     cli,
+		Name:       name,
 	}
 
 	//Setup Syncer and to handle events
@@ -142,5 +149,21 @@ func NewMatrixBot(user, pass string) (*MatrixBot, error) {
 		}
 	})
 
+	bot.RegisterCommand("help", 0, "Display this help", bot.handleCommandHelp)
 	return bot, nil
+}
+
+func (bot *MatrixBot) handleCommandHelp(message, room, sender string) {
+	//TODO make this a markdown table?
+
+	helpMsg := `The following commands are avaitible for this bot:
+
+Command			Power required		Explanation
+----------------------------------------------------------------`
+
+	for _, v := range bot.Handlers {
+		helpMsg = helpMsg + "\n!" + v.Pattern + "\t\t\t[" + strconv.Itoa(v.MinPower) + "]\t\t\t\t\t" + v.Help
+	}
+
+	bot.SendToRoom(room, helpMsg)
 }
